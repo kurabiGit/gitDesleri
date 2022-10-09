56782915ba46d25ddaafca4da65183d228e616c7
Batuhan Özdemir <ismailbatuhanozdemir@yandex.com>
2022-10-09 23:09:33 +0300

    Initial Commit

---
+++ gitDersi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitDersi
+//
+//  Created by Batuhan Özdemir on 9.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
